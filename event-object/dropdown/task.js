092472dd25c38d341cf945a3276ba6e15894d721
Neto test <borisovaed2014@yandex.ru>
2025-08-12 11:26:49 +0300

    first commit dropdown

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('.dropdown').forEach(dropdown => {
+    const value = dropdown.querySelector('.dropdown__value');
+    const list = dropdown.querySelector('.dropdown__list');
+    const items = dropdown.querySelectorAll('.dropdown__item a');
+
+    value.addEventListener('click', () => {
+        list.classList.toggle('dropdown__list_active');
+    });
+
+    items.forEach(item => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+            value.textContent = item.textContent;
+            list.classList.remove('dropdown__list_active');
+        });
+    });
+});
